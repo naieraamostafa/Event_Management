5cc24dbdb722082026bfb40b27f2d329738da75a
naieraamostafa <naieramostafa5@gmail.com>
2025-10-15 16:16:22 +0300

    Edit

--- Event_Management/Settings/EmailSettings.cs
+++ Event_Management/Settings/EmailSettings.cs
@@ -2,7 +2,7 @@
 {
     public class EmailSettings
     {
-        public string SmtpServer { get; set; } = " ";
+        public string SmtpServer { get; set; } = " "; //Edit
         public int SmtpPort { get; set; }
         public string Username { get; set; } = " ";
         public string Password { get; set; } = " ";
